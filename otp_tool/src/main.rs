7e0ce7aa4c6fbdd1d23bd3a72f97d8bd9e1eecf6
Martin Olivier <martin.olivier@live.fr>
2024-04-01 15:00:29 +0200

    feat: request command for otp_tools
    Signed-off-by: Martin Olivier <martin.olivier@live.fr>

--- otp_tool/src/main.rs
+++ otp_tool/src/main.rs
@@ -1,30 +1,32 @@
 use clap::{Parser, Subcommand, ValueEnum};
-use std::{fs::OpenOptions, io::Write, os::fd::AsRawFd};
+use std::{fs::OpenOptions, io::{Read, Write}, os::fd::AsRawFd};
 
-/// OTP tools
+/// OTP tools - manage your OTP devices
 #[derive(Debug, Parser)]
 #[clap(version, about, long_about = None)]
 pub struct App {
-    #[clap(long, value_parser)]
+    /// OTP device path
     device: String,
 
     #[clap(subcommand)]
     command: Command,
 }
 
-#[derive(Debug, Subcommand)]
+#[derive(Debug, Subcommand, PartialEq, Eq)]
 enum Command {
     /// Change the device mode
     SetMode {
-        /// Device mode, possible values: [list, algo]
+        /// Device mode
         #[arg(value_enum)]
-        #[clap(long, value_parser)]
         mode: Mode,
     },
+
+    /// Request a one time password
+    Request,
+
     /// Validate a one time password
     Validate {
         /// One time password
-        #[clap(long, value_parser)]
         otp: String,
     }
 }
@@ -36,14 +38,23 @@ pub enum Mode {
 }
 
 fn main() {
+    // parse arguments
     let args = App::parse();
 
-    let mut file = OpenOptions::new()
-        .write(true)
+    // open device
+    let mut device = match OpenOptions::new()
+        .read(if args.command == Command::Request { true } else { false })
+        .write(if args.command == Command::Request { false } else { true })
         .create(false)
-        .open(&args.device)
-        .unwrap();
+        .open(&args.device) {
+            Ok(device) => device,
+            Err(e) => {
+                eprintln!("Failed to open device '{}': {}", args.device, e);
+                std::process::exit(1);
+            }
+        };
 
+    // handle command
     match args.command {
         Command::SetMode { mode } => {
             let mode_id = match mode {
@@ -52,17 +63,32 @@ fn main() {
             };
 
             unsafe {
-                if libc::ioctl(file.as_raw_fd(), mode_id) == -1 {
+                if libc::ioctl(device.as_raw_fd(), mode_id) == -1 {
                     panic!("Ioctl failed");
                 }
             }
 
             println!("device '{}' has been set to mode '{:?}'", args.device, mode);
         },
+        Command::Request => {
+            let mut buffer = Vec::new();
+
+            if let Err(e) = device.read_to_end(&mut buffer) {
+                eprintln!("Failed to read from device '{}': {}", args.device, e);
+                std::process::exit(1);
+            }
+
+            let otp = String::from_utf8(buffer).unwrap();
+
+            println!("device '{}' returned requested otp: {}", args.device, otp);
+        },
         Command::Validate { otp } => {
-            match file.write_all(otp.as_bytes()) {
+            match device.write_all(otp.as_bytes()) {
                 Ok(()) => println!("otp has been approved by device '{}'", args.device),
-                Err(_) => panic!("otp has been rejected by device '{}'", args.device),
+                Err(_) => {
+                    eprintln!("otp has been rejected by device '{}'", args.device);
+                    std::process::exit(1);
+                }
             }
         },
     }
