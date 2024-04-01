266dbe89c21c1d9c96aa243d8199b0943975a577
Martin Olivier <martin.olivier@live.fr>
2024-04-02 00:44:02 +0200

    fix: code cleanup and doc
    Signed-off-by: Martin Olivier <martin.olivier@live.fr>

--- README.md
+++ README.md
@@ -1,20 +1,65 @@
 # OTP
 
-One Time Password kernel module
+A One Time Password management linux kernel module and it's additional tool
 
-## Build
-
-```sh
-make
-```
-
-## Install
+First, you will need to install `otp_tool`. To do so, make sure [cargo](https://www.rust-lang.org/tools/install) is installed on your system.
 
 ```sh
-insmod otp.ko list=p4ssw0rd,12345,kernel,qwerty
+cargo install --path otp_tool
 ```
 
-## Uninstall
+Then check the installation:
+
+```
+$ otp_tool
+OTP tools - manage your OTP devices
+
+Usage: otp_tool <DEVICE> <COMMAND>
+
+Commands:
+  set-mode  Change the device mode
+  request   Request a one time password
+  validate  Validate a one time password
+  help      Print this message or the help of the given subcommand(s)
+
+Arguments:
+  <DEVICE>  OTP device path
+
+Options:
+  -h, --help     Print help
+  -V, --version  Print version
+```
+
+## Build OTP module
+
+To build the OTP manager linux kernel module, enter the following command:
+
+```sh
+make -C otp
+```
+
+## Install OTP module
+
+Then, to install the OTP module, you can enter for example the following command:
+
+```sh
+insmod otp/otp.ko devices=3 pwd_list=p4ssw0rd,12345,kernel,qwerty
+```
+
+`devices`: create X OTP devices
+
+example with parameter `devices=3`:
+
+```
+$ ls /dev/ | grep otp
+otp0
+otp1
+otp2
+```
+
+`pwd_list`: password list used by devices when they are in `list` mode
+
+## Uninstall OTP module
 
 ```sh
 rmmod otp
@@ -22,48 +67,91 @@ rmmod otp
 
 ## Request OTP
 
-```sh
-cat /dev/otp ; echo
-```
+### Using CLI
 
 ```
+$ cat /dev/otpX ; echo
+p4ssw0rd
+```
+
+### Using otp_tools
+
+```
+$ otp_tool /dev/otpX request
 p4ssw0rd
 ```
 
 ## Validate OTP
 
+### Using CLI
+
 With a wrong otp:
 
-```sh
-echo -n "b@d p4ssw0rd" > /dev/otp
+```
+$ echo -n "b@d p4ssw0rd" > /dev/otp
 write error: Invalid argument
 ```
 
 With the otp
 
-```sh
-echo -n "p4ssw0rd" > /dev/otp
+```
+$ echo -n "p4ssw0rd" > /dev/otp
 ```
 
 A second time with the otp
 
-```sh
-echo -n "p4ssw0rd" > /dev/otp
+```
+$ echo -n "p4ssw0rd" > /dev/otp
 write error: Invalid argument
+```
+
+### Using otp_tools
+
+```
+$ otp_tool /dev/otpX validate p4ssw0rd
+otp has been approved by device '/dev/otpX'
 ```
 
 ## Display password list
 
-```sh
-cat /sys/module/otp/parameters/list
 ```
-
-```
+$ cat /sys/module/otp/parameters/pwd_list
 p4ssw0rd,12345,kernel,qwerty
 ```
 
 ## Edit password list
 
-```sh
-echo "nEw,BeTtEr,P@ssw0RdS" > /sys/module/otp/parameters/list
+```
+$ echo "nEw,BeTtEr,P@ssw0RdS" > /sys/module/otp/parameters/pwd_list
+```
+
+## Edit devices nb
+
+```
+echo 5 > /sys/module/otp/parameters/devices
+```
+
+```
+$ ls /dev/ | grep otp
+otp0
+otp1
+otp2
+otp3
+otp4
+```
+
+## Edit device mode
+
+Put a device in `list` mode (default):
+
+```
+$ otp_tool /dev/otpX set-mode list
+device '/dev/otpX' has been set to mode 'List'
+```
+
+Put a device in `algo` mode:
+
+```
+$ otp_tool /dev/otpX set-mode algo
+device '/dev/otpX' has been set to mode 'Algo'
 ```
--- otp/otp.c
+++ otp/otp.c
@@ -34,26 +34,36 @@ struct otp_state {
 	bool is_algo;
 };
 
+static struct otp_state otp_state_new(void)
+{
+	return (struct otp_state) {
+		.iterator = -1,
+		.already_validated = true,
+		.already_open = ATOMIC_INIT(DEV_NOT_USED),
+		.is_algo = false
+	};
+}
+
 // one time password data array
 struct otp_state otp_states[MAX_DEVICES];
 
 /* Parameters */
-static int list_argc;
-static char *list[4096] = { NULL };
+static int devices = 1;
+static char *pwd_list[4096] = { NULL };
+static int pwd_list_argc;
 
-int count = 1;
-
-int notify_count_param(const char *val, const struct kernel_param *kp)
+/* Parameters callback */
+int notify_devices_param(const char *val, const struct kernel_param *kp)
 {
-	int old_count = count;
+	int old_devices = devices;
 	int res = param_set_int(val, kp);
 
 	if (res != 0)
 		return -EINVAL;
 
-	// if the count is less than 1 or greater that MAX_DEVICES, we return an error
-	if (count < 1 || count > MAX_DEVICES) {
-		count = old_count;
+	// if devices is less than 1 or greater that MAX_DEVICES, we return an error
+	if (devices < 1 || devices > MAX_DEVICES) {
+		devices = old_devices;
 		return -EINVAL;
 	}
 
@@ -61,22 +71,17 @@ int notify_count_param(const char *val, const struct kernel_param *kp)
 	if (cls == NULL)
 		return 0;
 
-	pr_info("otp: count changed from %d to %d\n", old_count, count);
+	pr_info("otp: devices changed from %d to %d\n", old_devices, devices);
 
-	// if the new count is greater than the old count, we need to add devices, instead, if it is smaller, we need to remove devices
-	if (count > old_count) {
-		for (int i = old_count; i < count; i++) {
+	// if devices is greater than old devices, we need to add devices, instead, if it is smaller, we need to remove devices
+	if (devices > old_devices) {
+		for (int i = old_devices; i < devices; i++) {
 			device_create(cls, NULL, MKDEV(major, i), NULL, "%s%d", DEVICE_NAME, i);
-			otp_states[i] = (struct otp_state) {
-				.iterator = -1,
-				.already_validated = true,
-				.already_open = ATOMIC_INIT(DEV_NOT_USED),
-				.is_algo = false
-			};
+			otp_states[i] = otp_state_new();
 			pr_info("otp: device created at /dev/%s%d\n", DEVICE_NAME, i);
 		}
-	} else if (count < old_count) {
-		for (int i = count; i < old_count; i++) {
+	} else if (devices < old_devices) {
+		for (int i = devices; i < old_devices; i++) {
 			device_destroy(cls, MKDEV(major, i));
 			pr_info("otp: device deleted /dev/%s%d\n", DEVICE_NAME, i);
 		}
@@ -85,69 +90,16 @@ int notify_count_param(const char *val, const struct kernel_param *kp)
 	return 0;
 }
 
-const struct kernel_param_ops count_cb_ops = {
-	.set = &notify_count_param,
+const struct kernel_param_ops devices_cb_ops = {
+	.set = &notify_devices_param,
 	.get = &param_get_int,
 };
 
-module_param_cb(count, &count_cb_ops, &count, 0660);
-MODULE_PARM_DESC(count, "Number of devices to create");
+module_param_cb(devices, &devices_cb_ops, &devices, 0660);
+MODULE_PARM_DESC(devices, "Number of devices to create");
 
-module_param_array(list, charp, &list_argc, 0660);
-MODULE_PARM_DESC(list, "Passwords list");
-
-/* Prototypes */
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
-static ssize_t device_read(struct file *, char __user *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *);
-static long device_ioctl(struct file *, unsigned int, unsigned long);
-
-static struct file_operations dev_fops = {
-	.read = device_read,
-	.write = device_write,
-	.open = device_open,
-	.release = device_release,
-	.unlocked_ioctl = device_ioctl,
-};
-
-static int __init dev_init(void)
-{
-	major = register_chrdev(0, DEVICE_NAME, &dev_fops);
-
-	if (major < 0) {
-		pr_alert("Registering device failed with code %d\n", major);
-		return major;
-	}
-
-	pr_info("otp: major number assigned: %d\n", major);
-
-	cls = class_create(THIS_MODULE, DEVICE_NAME);
-
-	for (int i = 0; i < count; i++) {
-		device_create(cls, NULL, MKDEV(major, i), NULL, "%s%d", DEVICE_NAME, i);
-		otp_states[i] = (struct otp_state) {
-			.iterator = -1,
-			.already_validated = true,
-			.already_open = ATOMIC_INIT(DEV_NOT_USED),
-			.is_algo = false
-		};
-		pr_info("otp: device created at /dev/%s%d\n", DEVICE_NAME, i);
-	}
-
-	return 0;
-}
-
-static void __exit dev_exit(void)
-{
-	for (int i = 0; i < count; i++) {
-		device_destroy(cls, MKDEV(major, i));
-		pr_info("otp: device deleted /dev/%s%d\n", DEVICE_NAME, i);
-	}
-	class_destroy(cls);
-
-	unregister_chrdev(major, DEVICE_NAME);
-}
+module_param_array(pwd_list, charp, &pwd_list_argc, 0660);
+MODULE_PARM_DESC(pwd_list, "Passwords list");
 
 /* Methods */
 
@@ -158,11 +110,11 @@ static int device_open(struct inode *inode, struct file *file)
 {
 	int minor_number = iminor(file_inode(file));
 
+	/* Check if the device is free to use and swap values */
 	if (atomic_cmpxchg(&otp_states[minor_number].already_open, DEV_NOT_USED, DEV_USED))
 		return -EBUSY;
 
-	pr_info("otp: device opened\n");
-
+	/* Increment the usage count */
 	try_module_get(THIS_MODULE);
 
 	return 0;
@@ -185,51 +137,79 @@ static int device_release(struct inode *inode, struct file *file)
 }
 
 /*
- * Called when a process reads from the dev file
+ * Called by 'device_read' when the device mode is set to list
  */
-static ssize_t device_read(struct file *file,
-				char __user *buffer,
-				size_t length,
-				loff_t *offset)
+static ssize_t device_read_list(struct file *file,
+				char __user *buf,
+				size_t len,
+				loff_t *off)
 {
 	size_t string_length;
 	size_t bytes_to_read;
 
 	int minor_number = iminor(file_inode(file));
 
-	if (list_argc == 0)
+	if (pwd_list_argc == 0)
 		return -EINVAL;
 
 	// If a new read occurs, we need to create a new one time password
-	if (*offset == 0) {
+	if (*off == 0) {
 		otp_states[minor_number].iterator++;
-		if (otp_states[minor_number].iterator >= list_argc)
+		if (otp_states[minor_number].iterator >= pwd_list_argc)
 			otp_states[minor_number].iterator = 0;
 	}
 
-	string_length = strlen(list[otp_states[minor_number].iterator]);
-	bytes_to_read = min(length, (size_t)(string_length - *offset));
+	string_length = strlen(pwd_list[otp_states[minor_number].iterator]);
+	bytes_to_read = min(len, (size_t)(string_length - *off));
 
 	// If offset is beyond the end of the string, we have nothing more to read
-	if (*offset >= string_length) {
+	if (*off >= string_length) {
 		otp_states[minor_number].already_validated = false;
 		return 0;
 	}
 
 	// Copy data from kernel buffer to user buffer
-	if (copy_to_user(buffer, list[otp_states[minor_number].iterator] + *offset, bytes_to_read))
+	if (copy_to_user(buf, pwd_list[otp_states[minor_number].iterator] + *off, bytes_to_read))
 		return -EFAULT;
 
 	// Update the offset and number of bytes read
-	*offset += bytes_to_read;
+	*off += bytes_to_read;
 
 	return bytes_to_read;
 }
 
 /*
- * Called when a process writes to the dev file
+ * Called by 'device_read' when the device mode is set to algo
  */
-static ssize_t device_write(struct file *file, const char __user *buff,
+static ssize_t device_read_algo(struct file *file,
+				char __user *buf,
+				size_t len,
+				loff_t *off)
+{
+	// TODO: to implement
+	return -EINVAL;
+}
+
+/*
+ * Called when a process reads from the dev file
+ */
+static ssize_t device_read(struct file *file,
+				char __user *buf,
+				size_t len,
+				loff_t *off)
+{
+	int minor_number = iminor(file_inode(file));
+
+	if (otp_states[minor_number].is_algo)
+		return device_read_algo(file, buf, len, off);
+	else
+		return device_read_list(file, buf, len, off);
+}
+
+/*
+ * Called by 'device_write' when the device mode is set to list
+ */
+static ssize_t device_write_list(struct file *file, const char __user *buf,
 				size_t len, loff_t *off)
 {
 	static char kernel_buf[PAGE_SIZE];
@@ -240,10 +220,10 @@ static ssize_t device_write(struct file *file, const char __user *buff,
 	if (otp_states[minor_number].already_validated)
 		return -EINVAL;
 
-	if (otp_states[minor_number].iterator == -1 || otp_states[minor_number].iterator >= list_argc)
+	if (otp_states[minor_number].iterator == -1 || otp_states[minor_number].iterator >= pwd_list_argc)
 		return -EINVAL;
 
-	pwd_len = strlen(list[otp_states[minor_number].iterator]);
+	pwd_len = strlen(pwd_list[otp_states[minor_number].iterator]);
 
 	if (len > PAGE_SIZE)
 		return -EINVAL;
@@ -253,15 +233,39 @@ static ssize_t device_write(struct file *file, const char __user *buff,
 		return -EINVAL;
 
 	// get the data from the user
-	if (copy_from_user(kernel_buf, buff, len))
+	if (copy_from_user(kernel_buf, buf, len))
 		return -EFAULT;
 
 	// compare incoming data with the current password
-	if (strncmp(kernel_buf, list[otp_states[minor_number].iterator], pwd_len) == 0) {
+	if (strncmp(kernel_buf, pwd_list[otp_states[minor_number].iterator], pwd_len) == 0) {
 		otp_states[minor_number].already_validated = true;
 		return len;
 	} else
 		return -EINVAL;
+}
+
+/*
+ * Called by 'device_write' when the device mode is set to algo
+ */
+static ssize_t device_write_algo(struct file *file, const char __user *buf,
+				size_t len, loff_t *off)
+{
+	// TODO: to implement
+	return -EINVAL;
+}
+
+/*
+ * Called when a process writes to the dev file
+ */
+static ssize_t device_write(struct file *file, const char __user *buf,
+				size_t len, loff_t *off)
+{
+	int minor_number = iminor(file_inode(file));
+
+	if (otp_states[minor_number].is_algo)
+		return device_write_algo(file, buf, len, off);
+	else
+		return device_write_list(file, buf, len, off);
 }
 
 static long device_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
@@ -282,6 +286,47 @@ static long device_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	}
 
 	return 0;
+}
+
+static struct file_operations dev_fops = {
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release,
+	.unlocked_ioctl = device_ioctl,
+};
+
+static int __init dev_init(void)
+{
+	major = register_chrdev(0, DEVICE_NAME, &dev_fops);
+
+	if (major < 0) {
+		pr_alert("Registering device failed with code %d\n", major);
+		return major;
+	}
+
+	pr_info("otp: major number assigned: %d\n", major);
+
+	cls = class_create(THIS_MODULE, DEVICE_NAME);
+
+	for (int i = 0; i < devices; i++) {
+		device_create(cls, NULL, MKDEV(major, i), NULL, "%s%d", DEVICE_NAME, i);
+		otp_states[i] = otp_state_new();
+		pr_info("otp: device created at /dev/%s%d\n", DEVICE_NAME, i);
+	}
+
+	return 0;
+}
+
+static void __exit dev_exit(void)
+{
+	for (int i = 0; i < devices; i++) {
+		device_destroy(cls, MKDEV(major, i));
+		pr_info("otp: device deleted /dev/%s%d\n", DEVICE_NAME, i);
+	}
+	class_destroy(cls);
+
+	unregister_chrdev(major, DEVICE_NAME);
 }
 
 module_init(dev_init);
